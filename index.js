6d4e3d08c37c4fb7fae2fac597f25e54782a8f24
Krish-Makwana-1205 <202301103@daiict.ac.in>
2024-12-10 17:16:14 +0530

    The current aspects of the project are achieved

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const express =  require('express');
+const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// Serve static files (CSS, JS) from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the HTML file when the root URL is requested
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Tic-Tac-Toe-AI.html'));
+});
+
+// Start the server
+app.listen(8002, () => {
+    console.log('ears up');
+});
